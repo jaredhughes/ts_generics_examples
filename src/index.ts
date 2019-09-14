d9cebc8d12028455dc71ebfae51265baa161f061
Jared Hughes <j@redhugh.es>
2019-09-15 09:47:10 +1000

    Improve annotations

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,62 @@
 import { people, companies, Person, Company } from './data';
-import * as filter from './filter';
+import * as filterBy from './filter_by';
 
-console.log('ANY ======================\n');
-
-const person = filter.withoutGeneric(people, 'firstName', people[0].firstName);
-
-console.log(people[0].firstName, person);
-
-const company = filter.withoutGeneric(
-  companies,
-  'website',
-  companies[1].website
-);
-console.log(companies[1].website, company);
-
-console.log('PERSON ======================\n');
-
-const personTyped = filter.withGeneric<Person>(
+/*
+ * Invocation with implicit `any`
+ */
+const person = filterBy.withoutGeneric(
   people,
   'firstName',
   people[0].firstName
 );
 
-console.log(people[0].firstName, personTyped);
+console.log('Person:', person); // => any[]
 
-console.log('COMPANY ======================\n');
-
-personTyped.map(p => p.firstName);
-
-const companyTyped = filter.withGeneric<Company>(
+/*
+ * Invocation with implicit `any`
+ */
+const company = filterBy.withoutGeneric(
   companies,
   'website',
   companies[1].website
 );
 
-console.log(companies[1].website, companyTyped);
+console.log('Company:', company); // => any[]
 
-console.log('PROMISES ======================\n');
+/*
+ * Invocation utilising `Person` type passed as generic
+ */
+const personTyped = filterBy.withGeneric<Person>(
+  people,
+  'firstName', // => `keyof Person`
+  people[0].firstName
+);
 
+console.log('Person:', personTyped); // => Person[]
+
+// => typeof personTyped[0].firstName == 'string'
+
+/*
+ * Invocation utilising `Company` type passed as generic
+ */
+const companyTyped = filterBy.withGeneric<Company>(
+  companies,
+  'website', // => `keyof Company`
+  companies[1].website
+);
+
+console.log('Company:', companyTyped); // => Company[]
+
+/*
+ * Promise call, providing `Person` type for the
+ * resolve value generic
+ */
 const promise = new Promise<Person[]>(resolve => {
   setTimeout(() => resolve(people), 1000);
 });
 
-promise.then(people => people.map(p => console.log(p.firstName)));
+console.log('Resolved Person:', promise); // => Person[]
+
+promise.then((people: Person[]) =>
+  people.map((p: Person) => console.log(p.firstName))
+);
